96365972db23918b2b954002da4b9872a76f1623
Julian Escobar <magneticrex@gmail.com>
2020-06-26 21:01:14 -0300

    args

--- colorgrab.py
+++ colorgrab.py
@@ -1,28 +1,39 @@
 import pyscreenshot as ImageGrab
 from PIL import Image as img
+import sys
 
-RESET = '\033[0m'
-def get_color_escape(r, g, b, background=False):
-    return '\033[{};2;{};{};{}m'.format(48 if background else 38, r, g, b)
+if len(sys.argv) > 2:
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+    
+    RESET = '\033[0m'
+    def get_color_escape(r, g, b, background=False):
+        return '\033[{};2;{};{};{}m'.format(48 if background else 38, r, g, b)
 
-im = ImageGrab.grab(bbox=(20, 740, 22, 742))  # X1,Y1,X2,Y2
+    im = ImageGrab.grab(bbox=( x, y, x+2, y+2))  # X1,Y1,X2,Y2
 
-im.save('pixels.png')
+    im.save('pixels.png')
 
-imagen = img.open('pixels.png')
-pix = imagen.load()
-value1=hex(pix[1,1][0]).replace('0x','')
-value2=hex(pix[1,1][1]).replace('0x','')
-value3=hex(pix[1,1][2]).replace('0x','')
+    imagen = img.open('pixels.png')
+    pix = imagen.load()
+    value1=hex(pix[1,1][0]).replace('0x','')
+    value2=hex(pix[1,1][1]).replace('0x','')
+    value3=hex(pix[1,1][2]).replace('0x','')
 
-v1 = pix[1,1][0]
-v2 = pix[1,1][1]
-v3 = pix[1,1][2]
+    v1 = pix[1,1][0]
+    v2 = pix[1,1][1]
+    v3 = pix[1,1][2]
 
-rgb = '#'+value1+value2+value3
-print(rgb)
+    rgb = '#'+value1+value2+value3
+    print(rgb)
 
-print(get_color_escape(v1+30, v2+30, v3+30) 
-      + get_color_escape(v1, v2, v3, True)
-      + 'Fancy colors!' 
-      + RESET)
+    print(get_color_escape(v1+30, v2+30, v3+30) 
+        + get_color_escape(v1, v2, v3, True)
+        + 'Fancy colors!' 
+        + RESET)
+
+else:
+    print("not enough arguments passed")
+    print("usage: colorgrab.py <x-position> <y-position>")
+    exit
+    
